b3fd4fc1c81a6e6060a6a3087ae4b2a9b25c0ccf
Muhammad Waqas <waqasazamkml@gmail.com>
2017-03-03 17:39:33 +0500

    Method (findShortCodes) uses Matcher to find all short-codes inside the given text.

--- app/src/main/java/com/readfromfile/MainActivity.java
+++ app/src/main/java/com/readfromfile/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnRead, btnScan;
     TextView tvText;
-    EditText etScanString;
+//    EditText etScanString;
+    ListView lvShortCodes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,61 +29,73 @@ public class MainActivity extends AppCompatActivity {
         btnRead = (Button) findViewById(R.id.btnReadText);
         btnScan = (Button) findViewById(R.id.btnScanText);
         tvText = (TextView) findViewById(R.id.tvText);
-        etScanString = (EditText) findViewById(R.id.etScanString);
+//        etScanString = (EditText) findViewById(R.id.etScanString);
+        lvShortCodes = (ListView) findViewById(R.id.lvShortCodes);
 
         btnRead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BufferedReader reader = null;
-                StringBuilder text = new StringBuilder();
-                try {
-                    reader = new BufferedReader(
-                            new InputStreamReader(getAssets().open("wp_tips.txt")));
-
-                    // do reading, usually loop until end of file reading
-                    String mLine;
-                    while ((mLine = reader.readLine()) != null) {
-                        //process line
-                        text.append(mLine);
-                        text.append('\n');
-                    }
-                } catch (IOException e) {
-                    //log the exception
-                } finally {
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            //log the exception
-                        }
-                    }
-                }
-                tvText.setText(text);
+                readText();
             }
         });
 
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String myString = tvText.getText().toString();
+                findShortCodes();
+
+            }
+        });
+    }
+
+    public void readText(){
+
+        BufferedReader reader = null;
+        StringBuilder text = new StringBuilder();
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("wp_tips.txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                //process line
+                text.append(mLine);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        tvText.setText(text);
+
+    }
+
+    public void findShortCodes(){
+        String myString = tvText.getText().toString();
 //                Pattern myPattern = Pattern.compile("'(.*?)'"); // regular expression for any text contained inside single quotes ''
-                Pattern myPattern = Pattern.compile("\\{(.*?)\\}"); // regular expression for any text contained inside curly brackets{}
-                // brackets are escaped using backslashes \ but backslash itself is a special character in java's string
-                // so it is also escaped by backslash, making it a double backslash \\
+        Pattern myPattern = Pattern.compile("\\{(.*?)\\}"); // regular expression for any text contained inside curly brackets{}
+        // brackets are escaped using backslashes \ but backslash itself is a special character in java's string
+        // so it is also escaped by backslash, making it a double backslash \\
 
-                Matcher matcher = myPattern.matcher(myString);
+        Matcher matcher = myPattern.matcher(myString);
 
-                //This if condition finds only first occurrence of the provided pattern's match.
+        //This if condition finds only first occurrence of the provided pattern's match.
 //                if(matcher.find()){
 //                    Toast.makeText(MainActivity.this, matcher.group(), Toast.LENGTH_SHORT).show();
 //                }
 
-                //This loops through the input string and finds all occurrences of the provided pattern.
-                while(matcher.find()){
-                    Toast.makeText(MainActivity.this, matcher.group(), Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+        //This loops through the input string and finds all occurrences of the provided pattern.
+        while(matcher.find()){
+            Toast.makeText(MainActivity.this, String.valueOf(matcher.start()) + " value " + matcher.group() + String.valueOf(matcher.end()), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, , Toast.LENGTH_SHORT).show();
+        }
     }
 }
