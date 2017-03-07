528fecd714100f7e5232a60cb308b847407d115e
Muhammad Waqas <waqasazamkml@gmail.com>
2017-03-07 18:26:58 +0500

    Short-Code is being replaced now. Finally!

--- app/src/main/java/com/readfromfile/MainActivity.java
+++ app/src/main/java/com/readfromfile/MainActivity.java
@@ -10,13 +10,19 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnRead, btnScan;
+    Button btnRead, btnScanNReplace;
     TextView tvText, tvReplacedText;
+    StringBuilder text, replacedText;
+    HashMap<String, String> shortCodeExplanations; // HashMap containing short-codes as keys and their explanations as values
+    HashMap<String, String> shortCodesFound = null; // HashMap containing short-codes' count as a key and short-code itself as a value
+    String replacedString;
+    int shortCodesCount = 0;
     //    EditText etScanString;
     //ListView lvShortCodes;
 
@@ -26,11 +32,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnRead = (Button) findViewById(R.id.btnReadText);
-        btnScan = (Button) findViewById(R.id.btnScanText);
+        btnScanNReplace = (Button) findViewById(R.id.btnScanText);
         tvText = (TextView) findViewById(R.id.tvOriginalText);
         tvReplacedText = (TextView) findViewById(R.id.tvReplacedText);
 //        etScanString = (EditText) findViewById(R.id.etScanString);
         //lvShortCodes = (ListView) findViewById(R.id.lvShortCodes);
+
+        // HashMap containing short-codes as keys and their explanations as values
+        shortCodeExplanations = new HashMap<>();
+        shortCodeExplanations.put("{short_code}","This is explanation of short_code.");
+        shortCodeExplanations.put("{another_short_code}","This is explanation of another_short_code.");
 
         btnRead.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnScan.setOnClickListener(new View.OnClickListener() {
+        btnScanNReplace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findShortCodes();
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     public void readText() {
 
         BufferedReader reader = null;
-        StringBuilder text = new StringBuilder();
+        text = new StringBuilder();
         try {
             reader = new BufferedReader(
                     new InputStreamReader(getAssets().open("wp_tips.txt")));
@@ -80,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void findShortCodes() {
         String myString = tvText.getText().toString();
+        replacedString = tvText.getText().toString();
 //                Pattern myPattern = Pattern.compile("'(.*?)'"); // regular expression for any text contained inside single quotes ''
         Pattern myPattern = Pattern.compile("\\{(.*?)\\}"); // regular expression for any text contained inside curly brackets{}
         // brackets are escaped using backslashes \ but backslash itself is a special character in java's string
@@ -94,8 +106,12 @@ public class MainActivity extends AppCompatActivity {
 
         //This loops through the input string and finds all occurrences of the provided pattern.
         while (matcher.find()) {
+//            shortCodesCount+=1;
+//            shortCodesFound.put(String.valueOf(shortCodesCount),matcher.group());
             Toast.makeText(MainActivity.this, String.valueOf(matcher.start()) + " value " + matcher.group() + String.valueOf(matcher.end()), Toast.LENGTH_SHORT).show();
             //Toast.makeText(MainActivity.this, , Toast.LENGTH_SHORT).show();
+            replacedString = replacedString.replace(matcher.group(),shortCodeExplanations.get(matcher.group()));
+            tvReplacedText.setText(replacedString);
         }
     }
 }
